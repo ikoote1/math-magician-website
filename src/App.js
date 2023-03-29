c987cd0916af7bc3f40e8f25b1ec9c86abb6bd22
ikoote1 <ikootepreim@gmail.com>
2023-03-29 15:06:59 +0300

    Updated math magicians code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Display from './components/DisplayQuates';
-import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Navigation from './components/Navigation';
 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Routes>
-        <Route path="/" element={ <Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
         <Route path="/Quotes" element={<Display />} />
       </Routes>
