8e27d37929067642ee990c1e22c36856759980ac
ikoote1 <ikootepreim@gmail.com>
2023-03-29 12:44:57 +0300

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Calculator from './components/Calculator';
 import Display from './components/DisplayQuates';
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+  
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/calculator" element={<Calculator />} />
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+export default Navigation(){
+    return(
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/Quotes">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    )
+}
