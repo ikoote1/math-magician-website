cb3f0a7601ad4cc450affc625ff37bf354cfb381
ikoote1 <ikootepreim@gmail.com>
2023-03-29 13:44:49 +0300

    Updated math magicians code

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,19 @@ export default function Home(){
     return (
          <div className="home">
             <h2>Welcome to our page!</h2>
-            <p>Are you looking for a fun and interactive way to learn and practice mathematics? Look no further than Math Magician! Our application is designed to help you sharpen your math skills and become a true math wizard.
+            <p>
+                Are you looking for a fun and interactive way to learn and practice mathematics? Look no further than Math Magician! Our application is designed to help you sharpen your math skills and become a true math wizard.
+            </p>
+            <p>
+                With Math Magician, you can choose from a variety of topics including addition, subtraction, multiplication, division, and more. Our program offers a range of difficulty levels, from beginner to advanced, ensuring that you are always challenged and engaged. 
+            </p>
+            <p>
+                Whether you are a student looking to improve your grades or an adult seeking to enhance your mental agility, Math Magician is the perfect tool for you. Our user-friendly interface makes it easy to navigate and our dynamic graphics will keep you entertained as you learn.
 
-With Math Magician, you can choose from a variety of topics including addition, subtraction, multiplication, division, and more. Our program offers a range of difficulty levels, from beginner to advanced, ensuring that you are always challenged and engaged.
-
-Whether you are a student looking to improve your grades or an adult seeking to enhance your mental agility, Math Magician is the perfect tool for you. Our user-friendly interface makes it easy to navigate and our dynamic graphics will keep you entertained as you learn.
-
-So why wait? Start now your journey to becoming a math magician today</p>
+            </p>
+            <p>
+                So why wait? Start now your journey to becoming a math magician today
+            </p>
         </div>
     )
 }
