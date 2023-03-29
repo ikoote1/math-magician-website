b89849ba3b0020cdf233580341815bb6fee664e7
ikoote1 <ikootepreim@gmail.com>
2023-03-29 15:07:57 +0300

    Updated math magicians code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,24 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-export default function Navigation(){
-    return(
-        <div className='navbar'>
-        <h1>Math Magicians</h1>
-            <nav>
-        <ul className='nav'>
+export default function Navigation() {
+  return (
+    <div className="navbar">
+      <h1>Math Magicians</h1>
+      <nav>
+        <ul className="nav">
           <li>
-            <Link to="/" className='link'>Home</Link>
+            <Link to="/" className="link">Home</Link>
           </li>
           <li>
-            <div class="vl"></div>
-            <Link to="/calculator" className='link'>Calculator</Link>
+            <div className="vl" />
+            <Link to="/calculator" className="link">Calculator</Link>
           </li>
           <li>
-            <div class="vl"></div>
-            <Link to="/Quotes" className='link'>Quote</Link>
+            <div className="vl" />
+            <Link to="/Quotes" className="link">Quote</Link>
           </li>
         </ul>
       </nav>
-        </div>
-    )
+    </div>
+  );
 }
